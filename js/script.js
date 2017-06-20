5cf7d02e194603ca4be6ee598b7ebbeba49a4705
Anton Drozd <anton.drozd.95@gmail.com>
2017-06-20 22:36:35 +0300

    Вычистил ненужные комментарии

--- js/script.js
+++ js/script.js
@@ -15,71 +15,73 @@
   });
 })();
 
-(function() {
-  var form = document.querySelector('.response-form');
-  var successPopup = document.querySelector('.success-popup');
-  var failurePopup = document.querySelector('.failure-popup');
-  var closeBtn = [failurePopup.querySelector('.failure-popup__close-btn'),
-                  successPopup.querySelector('.success-popup__close-btn')];
-  var requiredFields = document.querySelectorAll('.response-form__required');
-  var valid = true;
-  var invalidFieldFirst;
+if (window.location.href.indexOf("form") > -1) {
+  (function() {
+    var form = document.querySelector('.response-form');
+    var successPopup = document.querySelector('.success-popup');
+    var failurePopup = document.querySelector('.failure-popup');
+    var closeBtn = [failurePopup.querySelector('.failure-popup__close-btn'),
+                    successPopup.querySelector('.success-popup__close-btn')];
+    var requiredFields = document.querySelectorAll('.response-form__required');
+    var valid = true;
+    var invalidFieldFirst;
 
-  function popupClose() {
-    closeBtn[+valid].focus();
-    closeBtn[+valid].addEventListener('click', function() {
-      if (valid) {
-        successPopup.classList.remove('success-popup--opened');
-        successPopup.classList.add('success-popup--closed');
-      } else {
-        failurePopup.classList.remove('failure-popup--opened');
-        failurePopup.classList.add('failure-popup--closed');
-        requiredFields[invalidFieldFirst].focus();
-      }
-      valid = true;
-    });
-
-    window.addEventListener('keydown', function(e) {
-      if (e.keyCode == 27) {
+    function popupClose() {
+      closeBtn[+valid].focus();
+      closeBtn[+valid].addEventListener('click', function() {
         if (valid) {
           successPopup.classList.remove('success-popup--opened');
           successPopup.classList.add('success-popup--closed');
         } else {
           failurePopup.classList.remove('failure-popup--opened');
           failurePopup.classList.add('failure-popup--closed');
+          requiredFields[invalidFieldFirst].focus();
         }
-        requiredFields[invalidFieldFirst].focus();
         valid = true;
-      }
-    });
-  };
+      });
 
-  form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    var j = 0;
-    for (var i = 0; i < requiredFields.length; i++) {
-      if (!requiredFields[i].value) {
-        valid = false;
-        requiredFields[i].classList.add('response-form__required--show');
-        j++;
-        if (j == 1) {
-          invalidFieldFirst = i;
+      window.addEventListener('keydown', function(e) {
+        if (e.keyCode == 27) {
+          if (valid) {
+            successPopup.classList.remove('success-popup--opened');
+            successPopup.classList.add('success-popup--closed');
+          } else {
+            failurePopup.classList.remove('failure-popup--opened');
+            failurePopup.classList.add('failure-popup--closed');
+          }
+          requiredFields[invalidFieldFirst].focus();
+          valid = true;
+        }
+      });
+    };
+
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      var j = 0;
+      for (var i = 0; i < requiredFields.length; i++) {
+        if (!requiredFields[i].value) {
+          valid = false;
+          requiredFields[i].classList.add('response-form__required--show');
+          j++;
+          if (j == 1) {
+            invalidFieldFirst = i;
+          }
         }
       }
-    }
-    if (valid) {
-      successPopup.classList.remove('success-popup--closed');
-      successPopup.classList.add('success-popup--opened');
-    } else {
-      failurePopup.classList.remove('failure-popup--closed');
-      failurePopup.classList.add('failure-popup--opened');
-    }
-    popupClose();
-  });
-
-  requiredFields.forEach(element => {
-    element.addEventListener('focus', function() {
-      element.classList.remove('response-form__required--show');
+      if (valid) {
+        successPopup.classList.remove('success-popup--closed');
+        successPopup.classList.add('success-popup--opened');
+      } else {
+        failurePopup.classList.remove('failure-popup--closed');
+        failurePopup.classList.add('failure-popup--opened');
+      }
+      popupClose();
     });
-  });
-})();
+
+    requiredFields.forEach(element => {
+      element.addEventListener('focus', function() {
+        element.classList.remove('response-form__required--show');
+      });
+    });
+  })();
+}
